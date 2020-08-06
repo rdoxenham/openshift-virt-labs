5837d1d9461aaea983c3ad0202e7331341c73f70
Rhys Oxenham <rdoxenham@gmail.com>
2020-08-06 22:05:43 +0100

    Replaced the cleanup of disks section to actually wipe the disks where bash cannot find the files as they are owned by root

--- cleanup.sh
+++ cleanup.sh
@@ -5,12 +5,13 @@ for i in master1 master2 master3 worker1 worker2 worker3 bastion bootstrap
 do
 	sudo virsh destroy ocp4-$i
 	sudo virsh undefine ocp4-$i
+	sudo rm -f /var/lib/libvirt/images/ocp4-$i.qcow2
+	sudo rm -f /var/lib/libvirt/images/ocp4-$i-osd1.qcow2
+	sudo rm -f /var/lib/libvirt/images/ocp4-$i-osd2.qcow2
 done
 
 sudo rm -f node-configs/*
 sudo rm -rf generated/
-
-sudo rm -f /var/lib/libvirt/images/ocp4*.qcow2
 
 sudo virsh net-destroy ocp4-net
 sudo virsh net-undefine ocp4-net
